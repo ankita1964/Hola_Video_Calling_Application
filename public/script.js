943cf76a78dec7ea981115df15f9b823c68e61df
Ankita Jain <ankitajain@Ankitas-MacBook-Pro.local>
2021-06-21 14:57:46 +0530

    Server and Peer working with minimum 2 user on call

---
+++ public/script.js
@@ -0,0 +1,58 @@
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+var peer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+
+let myVideoStream;
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+
+
+    socket.on('user-connected', (userId) => {
+        setTimeout( () => {
+            connectToNewUser(userId, stream);
+        }, 1000);
+    })
+})
+
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id); 
+})
+
+ 
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    console.log("Connect ki request bhj di yaha se");
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    })
+}
+
+const addVideoStream = (video,stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video);
+}
+
